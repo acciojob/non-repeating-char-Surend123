16b2b3b7d699c57f0a908ee50fe72e5041c78004
Surendra <sky958447@gmail.com>
2025-01-28 15:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let map = new Map();
+	for(let ch of str){
+		if(!map.has(ch)){
+			map.set(ch,1);
+		}else{
+			map.set(ch,map.get(ch)+1);
+		}
+	}
+	for(let ch of str){
+		if(map.get(ch)==1){
+			return ch;
+		}
+	}
+	
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
